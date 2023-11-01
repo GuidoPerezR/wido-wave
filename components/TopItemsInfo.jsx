f916815ef02cbf60c7882da884329eca100e4571
Guido Perez Ruiz <guidopr27@hotmail.com>
2023-11-01 13:27:16 -0600

    Arreglando error

--- components/TopItemsInfo.jsx
+++ components/TopItemsInfo.jsx
@@ -2,14 +2,15 @@ import TrackItem from "./TrackItem";
 
 function TopItemsInfo({ topItems }) {
   return (
-    <div className="d-flex flex-column gap-3 mb-3">
+    <div className="d-flex flex-column gap-3 mb-5">
       <h2 className="text-center"> Top of Last 4 Weeks</h2>
       {topItems.length > 0 ? (
         topItems.map((item) => <TrackItem key={item.id} item={item} />)
       ) : (
         <div>
-          <h3 className="text-center mt-4 text-secondary">Listen your favorite songs to view this info</h3>
-
+          <h3 className="text-center mt-4 text-secondary">
+            Listen your favorite songs to view this info
+          </h3>
         </div>
       )}
     </div>
